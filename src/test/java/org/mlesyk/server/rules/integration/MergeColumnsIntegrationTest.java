2b0c9c85c50e9151d9e8ec80674d3031853f3281
Maks Lesyk <maksym@lesyk.org>
2017-09-07 23:16:20 +0300

    Updated integration test

--- src/test/java/org/mlesyk/server/rules/integration/MergeColumnsIntegrationTest.java
+++ src/test/java/org/mlesyk/server/rules/integration/MergeColumnsIntegrationTest.java
@@ -2,9 +2,10 @@ package org.mlesyk.server.rules.integration;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.mlesyk.server.rules.ChangeColumnPosition;
-import org.mlesyk.server.rules.Clone;
+import org.mlesyk.server.rules.*;
 import org.mlesyk.server.rules.helpers.AbstractRuleTestRunner;
+import org.mlesyk.server.utils.MathUtil;
+import org.mlesyk.server.utils.RegexUtil;
 
 /**
  * Created by Maks on 02.09.2017.
@@ -41,6 +42,53 @@ public class MergeColumnsIntegrationTest extends AbstractRuleTestRunner {
 
         System.out.println("Test " + new Object(){}.getClass().getEnclosingMethod().getName());
         Assert.assertEquals(runMergeTest(firstColumnId, secondColumnId, csvDataResult, cloneRule), true);
+    }
+
+    @Test
+    public void testMergeThenDelete() {
+        String[] csvDataResult = {
+                "column3,column4",
+                "data3,data4"
+        };
+        int firstColumnId = 0;
+        int secondColumnId = 1;
+
+        Delete deleteRule = new Delete(firstColumnId, manager.getOutputColumns());
+
+        System.out.println("Test " + new Object(){}.getClass().getEnclosingMethod().getName());
+        Assert.assertEquals(runMergeTest(firstColumnId, secondColumnId, csvDataResult, deleteRule), true);
+    }
+
+    @Test
+    public void testMergeThenMerge() {
+        String[] csvDataResult = {
+                "column1 column2 column3,column4",
+                "data1 data2 data3,data4"
+        };
+        int firstColumnId = 0;
+        int secondColumnId = 1;
+        int thirdColumnId = 2;
+
+        MergeColumns mergeRule = new MergeColumns(firstColumnId, thirdColumnId, manager.getOutputColumns());
+
+        System.out.println("Test " + new Object(){}.getClass().getEnclosingMethod().getName());
+        Assert.assertEquals(runMergeTest(firstColumnId, secondColumnId, csvDataResult, mergeRule), true);
+    }
+
+    @Test
+    public void testMergeThenRegex() {
+        String[] csvDataResult = {
+                "data1 data2,data3,data4"
+        };
+        int firstColumnId = 0;
+        int secondColumnId = 1;
+        int condition = RegexUtil.CONTAINS;
+        String searchData = "data";
+
+        RegexFilter regexRule = new RegexFilter(firstColumnId, condition, searchData, manager.getOutputColumns());
+
+        System.out.println("Test " + new Object(){}.getClass().getEnclosingMethod().getName());
+        Assert.assertEquals(runMergeTest(firstColumnId, secondColumnId, csvDataResult, regexRule), true);
     }
 
 }
